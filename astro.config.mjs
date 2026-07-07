2c05a1024dd429c907ef9fc170191574e095789a
David Wagner <david.wagner@etu.ec-lyon.fr>
2026-07-07 18:49:25 +0200

    correction du lien de prévisualisation pour le partage via linkedin notamment: voir baselayout (les "og")

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import icon from 'astro-icon';
 import preact from "@astrojs/preact";
 
 export default defineConfig({
-  site: "https://davzzzd.netlify.app/",
+  site: "https://david-wagner.netlify.app/",
   integrations: [preact(), icon()],
 });

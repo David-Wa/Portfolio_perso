91461d2c75544f7243c6e6787f772c67e9286771
David Wagner <david.wagner@etu.ec-lyon.fr>
2026-07-09 17:45:21 +0200

    github sites

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import icon from 'astro-icon';
 import react from "@astrojs/react";
 
 export default defineConfig({
-  site: "https://david-wagner.netlify.app/",
+  site: "https://github.com/David-Wa",
+  base: '/Portfolio_perso', // uniquement si le repo n'est PAS TON-PSEUDO.github.io
   integrations: [ icon(), react()],
 });

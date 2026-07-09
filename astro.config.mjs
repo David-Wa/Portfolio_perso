80b28976484dfa8e62e7edd1f211cff878ec02bb
David Wagner <david.wagner@etu.ec-lyon.fr>
2026-07-09 18:11:44 +0200

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import icon from 'astro-icon';
 import react from "@astrojs/react";
 
 export default defineConfig({
-  site: "https://david-wa.github.io",
-  base: '/Portfolio_perso', // uniquement si le repo n'est PAS TON-PSEUDO.github.io
+  site: "https://portfolio-perso-eta.vercel.app/",
+  /*base: '/Portfolio_perso', // uniquement si le repo n'est PAS TON-PSEUDO.github.io*/
   integrations: [ icon(), react()],
 });

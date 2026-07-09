4997bfbf77e760fb1ffdbf84ddd9fa6b7a277a00
David Wagner <david.wagner@etu.ec-lyon.fr>
2026-07-09 18:01:41 +0200

    changmeent adresse site dans config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import icon from 'astro-icon';
 import react from "@astrojs/react";
 
 export default defineConfig({
-  site: "https://github.com/David-Wa",
+  site: "https://david-wa.github.io",
   base: '/Portfolio_perso', // uniquement si le repo n'est PAS TON-PSEUDO.github.io
   integrations: [ icon(), react()],
 });

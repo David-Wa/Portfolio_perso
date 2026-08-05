670e9771f5e42ff90c77491cce65ee08e4dc2c15
David Wagner <david.wagner@etu.ec-lyon.fr>
2026-08-05 12:57:12 +0200

    amélioration header

--- src/components/DynamicBackground/GradientBg.jsx
+++ src/components/DynamicBackground/GradientBg.jsx
@@ -16,7 +16,6 @@ export default function GradientBg({color1="#DBDFAC", color2="#250902", color3="
       <ShaderGradientCanvas lazyLoad={false}>
 <ShaderGradient
   animate="on"
-  frameloop="demand"
   control="props"
   enableCameraUpdate={false}
   axesHelper="off"
@@ -36,7 +35,7 @@ export default function GradientBg({color1="#DBDFAC", color2="#250902", color3="
   gizmoHelper="hide"
   grain="on"
   lightType="3d"
-  pixelDensity={0.3}
+  pixelDensity={0.01}
   positionX={0}
   positionY={0}
   positionZ={0}

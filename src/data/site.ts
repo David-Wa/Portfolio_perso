f8734482176d505baba6f66c3b6fa3592d90735f
David Wagner <david.wagner@etu.ec-lyon.fr>
2026-08-10 13:32:04 +0200

    hauteur language corrigée

--- src/data/site.ts
+++ src/data/site.ts
@@ -2,13 +2,13 @@ export const siteContent = {
   fr: {
     footerBrand:"/tag.svg",
     footerLinks: [
-      { label: 'David Wagner 2026' },
+      { label: 'David Wagner 2026', href: "https://www.linkedin.com/in/david-wagner-ecl/" },
     ],
   },
   en: {
     footerBrand:"/tag.svg",
     footerLinks: [
-      { label: 'David Wagner 2026' },
+      { label: 'David Wagner 2026',  href: "https://www.linkedin.com/in/david-wagner-ecl/" },
     ],
   },
 };

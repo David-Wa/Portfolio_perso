4dfd4960ba6ceba59334da59d832521112b0d309
David Wagner <david.wagner@etu.ec-lyon.fr>
2026-08-11 12:24:36 +0200

    pixeldensity modifiée

--- src/components/DynamicBackground/GradientBg.jsx
+++ src/components/DynamicBackground/GradientBg.jsx
@@ -1,8 +1,6 @@
 import { ShaderGradientCanvas, ShaderGradient } from '@shadergradient/react'
 
-
 export default function GradientBg({color1="#DBDFAC", color2="#250902", color3="#DBDFAC"}) {
-
   return (
     <section
       style={{
@@ -13,49 +11,45 @@ export default function GradientBg({color1="#DBDFAC", color2="#250902", color3="
         pointerEvents: 'none'
       }}
     >
-      <ShaderGradientCanvas lazyLoad={false}>
-<ShaderGradient
-  animate="on"
-  control="props"
-  enableCameraUpdate={false}
-  axesHelper="off"
-  brightness={1.2}
-  cAzimuthAngle={250}
-  cDistance={1.5}
-  cPolarAngle={140}
-  cameraZoom={16.51}
-  color1={color1}
-  color2={color2}
-  color3={color3}
-  destination="onCanvas"
-  embedMode="off"
-  //format="gif"
-  fov={45}
-  frameRate={10}
-  gizmoHelper="hide"
-  grain="on"
-  lightType="3d"
-  pixelDensity={0.01}
-  positionX={0}
-  positionY={0}
-  positionZ={0}
-  range="disabled"
-  rangeEnd={40}
-  rangeStart={0}
-  reflection={0.5}
-  rotationX={180}
-  rotationY={90}
-  rotationZ={140}
-  shader="defaults"
-  type="sphere"
-  uAmplitude={7}
-  uDensity={4.4}
-  uFrequency={5.5}
-  uSpeed={0.03}
-  uStrength={0.3}
-  uTime={0}
-  wireframe={false}
-/>
+      <ShaderGradientCanvas
+        lazyLoad={false}
+        pixelDensity={0.8}
+        fov={45}
+      >
+        <ShaderGradient
+          animate="on"
+          control="props"
+          enableCameraUpdate={false}
+          brightness={1.2}
+          cAzimuthAngle={250}
+          cDistance={1.5}
+          cPolarAngle={140}
+          cameraZoom={16.51}
+          color1={color1}
+          color2={color2}
+          color3={color3}
+          grain="on"
+          lightType="3d"
+          positionX={0}
+          positionY={0}
+          positionZ={0}
+          range="disabled"
+          rangeEnd={40}
+          rangeStart={0}
+          reflection={0.5}
+          rotationX={180}
+          rotationY={90}
+          rotationZ={140}
+          shader="defaults"
+          type="sphere"
+          uAmplitude={7}
+          uDensity={4.4}
+          uFrequency={5.5}
+          uSpeed={0.03}
+          uStrength={0.3}
+          uTime={0}
+          wireframe={false}
+        />
       </ShaderGradientCanvas>
     </section>
   );
